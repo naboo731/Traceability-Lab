8319d253f0c4e00f35334ba57e970f2e838e4186
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 15:55:41 -0500

    updated pathx3

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public'))
+// })
+app.use('/',express.static(path.join(__dirname, 'public')))
 
 
 
