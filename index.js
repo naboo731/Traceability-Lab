3d0299ddcb9af3eeed6b9fb9b8972cc49ac2157c
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 15:38:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public'))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 4545
+app.listen(port, () => {
+    console.log(`Are you there dad? On port ${port}`)
+})
