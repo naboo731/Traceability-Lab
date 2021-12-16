38c71002ee141bc5433e30ee332f76586c50e399
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 16:01:42 -0500

    updated path for cssx2

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/styles.css'))
-})
+app.use('/',express.static(path.join(__dirname, 'public')))
 
 
 
