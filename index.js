9486c83d0b1036f63cdf13a49f5b82f725dc97e4
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 15:58:29 -0500

    updated path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 // app.use('/',express.static(path.join(__dirname, 'public')))
 
