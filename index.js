77d5e769daa8964a61c8171e0e3c79d8ffdda691
Nichole Boothman <nbooth731@gmail.com>
2021-12-16 17:45:35 -0500

    updated path

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const rollbar = new Rollbar({
 })
 app.use(express.json())
 
-app.use('/',express.static(path.join(__dirname, 'public')))
+app.use('/static',express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
